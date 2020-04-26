28211771259de038e2414796cac18cb1f7893b37
Tiesco2 <dtiekoura@yahoo.fr>
2020-04-26 16:57:32 +0200

    contact-list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import './App.css';
 import Contact from'./Components/Contact';
+import ContactList from './Components/ContactList';
 
 
 
@@ -12,11 +13,14 @@ class App extends Component{
     return ( 
      
           <div className="App"> 
+          
             <Contact name="Myrtle Porter" avatar="https://randomuser.me/api/portraits/women/3.jpg" status />
           
           <Contact name="Louella Simmons" avatar="https://randomuser.me/api/portraits/women/76.jpg" />
         
         <Contact name="Julia Murray" avatar="https://randomuser.me/api/portraits/women/62.jpg" status />
+        
+        <ContactList />
       </div>
     );
   }
