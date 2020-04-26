d252f47ef61dcda97420834c5a6e39202ec77b05
Tiesco2 <dtiekoura@yahoo.fr>
2020-04-27 00:35:33 +0200

    move status

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React,{Component} from 'react';
-import './App.css';
-import Contact from'./Components/Contact';
-import ContactList from './Components/ContactList';
+import ContactList from "./Components/ContactList";
 
 
 
-
-
-class App extends Component{
+class App extends Component {
   
   render(){
     return ( 
      
           <div className="App"> 
-          
-            <Contact name="Myrtle Porter" avatar="https://randomuser.me/api/portraits/women/3.jpg" status />
-          
-          <Contact name="Louella Simmons" avatar="https://randomuser.me/api/portraits/women/76.jpg" />
-        
-        <Contact name="Julia Murray" avatar="https://randomuser.me/api/portraits/women/62.jpg" status />
-        
-        <ContactList />
-      </div>
+            <ContactList />
+       
+          </div>
     );
   }
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,42 +1,40 @@
 import React,{Component} from 'react';
-import './Contact.css';
-import PropTypes from 'prop-types';
+import "./Contact.css";
+import PropTypes from "prop-types";
 
 
 
 
-class Contact extends Component{
+class Contact extends Component {
   
-  render(){
+  render(props){
     return (
       <div className="Contact">    
       <div>
-           <img src={this.props.avatar} alt="avatar" className="avatar" />
+           <img src={this.props.avatar} alt= "avatar" className= "avatar" />
       </div>
       <div>
             <div className="status"> 
 
-                 <p className="name">{this.props.name}</p>
+                 <p className="name">{this.props.name} </p>
 
-                 <div className= { this.props.status ?"status-online" : "status-offline"} />
+                 <div className= {this.props.online ? "status-online" : "status-offline"} />
 
-                 <p className="status-text"> { this.props.status ? "online" : "offline"}</p>
-
+                 <p className="status-text"> {this.props.online ? "online" : "offline"} </p>
+               
            </div>
 
        </div>
+      
 </div>
 );  
 }
 }
 Contact.propTypes = {  
-  name: PropTypes.string ,
-  status: PropTypes.string,
-  avatar: PropTypes.string
+  name: PropTypes.string.isRequired,
+  online: PropTypes.bool.isRequired,
+  avatar: PropTypes.string.isRequired
 };
-
-                                             
-
 
 
 export default Contact
